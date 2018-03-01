3fb9e4e5931cd82e9bbc71b8cbcf160b456dc498
Rage343 <wh48082@yahoo.com>
2018-03-01 19:23:33 -0400

    Queue data structure and update readme

---
+++ exercises/Queue.js
@@ -0,0 +1,7 @@
+/*
+The Queues (data structure) is like an array except there no duplicate items and their values are in any particular order. 
+The typical use for Sets is to simply check for a particular item. 
+
+*/
+
+console.log('====================Queues======================');
--- index.js
+++ index.js
@@ -3,3 +3,6 @@ require('./exercises/Stacks');
 
 //Sets Data Structure 
 require('./exercises/Sets');
+
+//Queue Data Structure 
+require('./exercises/Queue');
