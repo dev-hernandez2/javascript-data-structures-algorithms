07ec8f80b6d7f97dd2c53b3fcdcf5255b5f5d76b
Rage343 <wh48082@yahoo.com>
2018-03-01 19:27:05 -0400

    Queue data structure and update readme

--- exercises/Queue.js
+++ exercises/Queue.js
@@ -1,6 +1,5 @@
 /*
-The Queues (data structure) is like an array except there no duplicate items and their values are in any particular order. 
-The typical use for Sets is to simply check for a particular item. 
+The Queues (data structure) is similar to stack, but a queue is first in and first out 
 
 */
 
