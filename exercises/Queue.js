42a5b6da5baa238f09b1a38fb64fbecd8a634355
Rage343 <wh48082@yahoo.com>
2018-03-01 19:46:50 -0400

    Queue data structure

--- exercises/Queue.js
+++ exercises/Queue.js
@@ -4,3 +4,54 @@ The Queues (data structure) is similar to stack, but a queue is first in and fir
 */
 
 console.log('====================Queues======================');
+
+
+class Queue {
+
+    constructor() {
+        this.collection = [];
+    }
+
+    //Helpe print the collection 
+    print() {
+        console.log(this.collection);
+    }
+
+    // will push the first item into the queue
+    enqueue(element) {
+        this.collection.push(element);
+    }
+
+    //this will take the item of the queue
+    dequeue() {
+        //.shift pull off the element of the array and then is will return it
+        return this.collection.shift();
+    }
+
+    //it returns what it  item in the front of the array without removing it from the array
+    front() {
+        return this.collection[0];
+    }
+
+    //return the size of the collection
+    size() {
+        return this.collection.length;
+    }
+
+    //check if is empty
+    isEmpty() {
+        return (this.collection.length === 0);
+    }
+}
+
+
+const q = new Queue();
+
+q.enqueue('a');
+q.enqueue('b');
+q.enqueue('c');
+
+q.print();
+q.dequeue();
+console.log(q.front());
+q.print();
